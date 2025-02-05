d822803037f65ff517447f80e0f307d23ad7e9e6
Björn Beuthien <bjoern.beuthien@epshl.de>
2025-02-05 14:47:22 +0100

    * Add example for parsing xml

---
+++ parsing/parsing_example_xml.py
@@ -0,0 +1,23 @@
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('person.xml')
+root = tree.getroot()
+
+print(root.tag)
+
+# You can iterate through all direct children
+for child in root:
+    print(child.tag)
+
+for person in root.findall('person'):
+    if person.find('vorname').text == 'Anna':
+        address = person.find('adresse')
+        print(address.find('strasse').text)
+        address.find('strasse').text = 'Neue Strasse'
+    if person.find('vorname').text == 'David':
+        root.remove(person)
+
+ET.dump(tree)
+
+
+
